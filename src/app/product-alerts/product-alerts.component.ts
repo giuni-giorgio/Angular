000a0a7c9f505afef9f330ce4ea9c7637274694f
giuni-giorgio <private.giuni.giorgio@gmail.com>
2020-01-21 09:50:59 +0100

    1 file modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Input } from '@angular/core';
-import { Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
